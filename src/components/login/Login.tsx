26a49f89bb00daa40ccb8e9dd78d2a4ffc24e97c
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-10 19:54:31 +0900

    [기능] Login UI 컴포넌트 제작

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <>
+      <button type="button">
+        GitHub 로그인
+      </button>
+    </>
+  );
+}
+
+export default Login;
